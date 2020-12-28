e09e583313eddac72db66a37fcd3ee8a585a1af1
Ali <wizard2070@gmail.com>
2020-12-28 20:27:47 +0330

    fifth

--- JS Nokat.js
+++ JS Nokat.js
@@ -63,7 +63,130 @@ $(document).ready(function () {
             });
 
 
-            this.meta_keyword.splice(index, 1); // remove from array
+if(!isNaN(val.featured_image))
 
-			 
+
+ const { min, max } = list_date.reduce((o, d) => {
+        o.min = (o.min > d.min_price ? d.min_price : o.min);
+        o.max = (o.max < d.max_price ? d.max_price : o.max);
+        return o;
+      }, { min: Number.MAX_SAFE_INTEGER, max: -1 });
+
+
+      const [todayFullYear, todayMonth, todayDate] = today.toLocaleString('fa').split('¡')[0].split('/');
+	  
+	  
+	  
+	  groups[k].push({ min_price: d.min_price, max_price: d.max_price });
+	  minPrice = Math.min(...groups[dateStr].map(o => o.min_price));
+      maxPrice = Math.max(...groups[dateStr].map(o => o.max_price));
+      avgPrice = parseFloat(((maxPrice + minPrice) / 2).toFixed(2));
+
+
+if (staticSites) {
+      sitesResponse = { data: staticSites };
+      await Promise.delay(1500);
+    } else {
+      sitesResponse = await axios.get(`/api/v1/products/${product._key}/sites`);
+    }
+	
+	var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+  var citrus = fruits.slice(1, 3);
+  Orange,Lemon
+  
+  
+  $('.close_filter_tag').click(function () {
+		let li_element = $(this).parents('li');
+		li_element.remove();
+	});
+	
+	
+			if (value === ''){
+			$(this).siblings('.invalid-feedback').css('display', 'block');
+			is_name_validate = false;
+		}
+		
+		
+		
+	$('.signup_frm').change(); // call programitically
+	$('.signup_frm').on('change' , function () {})
+
+		let urlRejex = /^http:\/\/|(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+		let is_url_valid = urlRejex.test(value);
+
+	$.ajax({
+				url: '/api/v1/users/forms/report',
+				type: 'POST',
+				data: { 'report_type':'shopSignup' ,'full_name':name+family ,'email':email ,'reason':link_phone_desc ,'shop_name':shop_name  },
+				dataType: 'JSON',
+				success: function (data , xhr) {
+					if (xhr === 'success'){
+						showSnackBar('green' , 'ãÔÎÕÇÊ ÔãÇ ÇÑÓÇá ÔÏ. Èå ÒæÏí ÈÇ ÔãÇ ÊãÇÓ ãííÑíã');
+						resetSignUpForm();
+					} else {
+						showSnackBar('red' , xhr);
+					}
+				}, error:function (err) {
+					//console.log(err);
+				}, complete:function () {
+					$('#signup_frm_submit').find('i').css('visibility', 'hidden').removeClass('fa-spin');
+				},
+	timeout:10000
+			});
+			
+			
+			
+			document.getElementById("myForm").reset();
+			
+			let value = $(this).val().toLowerCase();
+		$("#sitesCollapse").find('.form-check').filter(function() {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+		
+		
+		// copyToClipboard //
+function copyToClipboard(element) {
+ 			var $temp = $("<input>");
+ 			$("body").append($temp);
+ 			$temp.val($(element).text()).select();
+ 			document.execCommand("copy");
+ 			$temp.remove();
+ 			//$('div.toast').css('display', 'block');
+ 			$('.tooltip-inner').text('ÑãÒ ÏÇäáæÏ ˜í ÔÏ');
+ }
+ 
+		
+		
+		$('#shops_search').trigger('input'); // input event trigger manually
+		
+		
+		
+			$('#shops_list_search').on('input' , function () {
+		window.scrollTo(0,document.body.scrollHeight);
+
+
+
+// Back to top btn
+	$(window).scroll(function(event) {
+		if ($(this).scrollTop() > 600) {
+			$('#scroll_to_top_btn').css('transform', 'translateX(125px) rotateZ(360deg)');
+		} else {
+			$('#scroll_to_top_btn').css('transform', 'translateX(-125px) rotateZ(-360deg)');
+		}
+	});
+	$('#scroll_to_top_btn').on('click', function () {
+		$('html,body').animate({
+			scrollTop: 0
+		}, 0);
+	});
+	
+	
+	$.fn.digits = function () {
+		return this.each(function () {
+			$(this).text($(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+		})
+	};
+	$('.digits').digits();
+
+	  
 })
