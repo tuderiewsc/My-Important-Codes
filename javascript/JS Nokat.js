3b18730997d812da1a5d21a8f1a3c802f3bf9fdf
Ali <wizard2070@gmail.com>
2021-01-17 20:36:55 +0330

    18th

--- javascript/JS Nokat.js
+++ javascript/JS Nokat.js
@@ -592,3 +592,42 @@ var data = {
 // wordpress ajax
 
 
+function bom() {
+            var w = window.innerWidth
+                || document.documentElement.clientWidth
+                || document.body.clientWidth;
+
+            var h = window.innerHeight
+                || document.documentElement.clientHeight
+                || document.body.clientHeight;
+
+            var x = document.getElementById('demo');
+            x.innerHTML = "Brpwser inner width : " + w + ", Height : " + h;
+        }
+		
+// Countdown timer
+var countDownDate = new Date("2021-07-20 23:59:59").getTime();
+
+        var x = setInterval(function (){
+            var now = new Date().getTime();
+
+            var distance = countDownDate - now;
+
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+            var minutes =Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds =Math.floor((distance % (1000 * 60)) / 1000);
+
+            document.getElementById('demo')
+            .innerHTML = days + "Day "+hours+" Houre "+minutes+" Minutes "+seconds +" Second ";
+
+            if(distance < 0)
+            {
+                clearInterval(x);
+                document.getElementById('demo')
+            .innerHTML = "تمام";
+            }
+        },1000);
+// Countdown timer
+
+
